7932620d0f692c2407f61207ad21997e6e58aa76
Juri Di Rocco <juri.dirocco@univaq.it>
2025-04-23 11:31:37 +0200

    Minor change to tests

--- src/test/java/it/univaq/disim/mosaico/wp2/repository/ModelRepositoryTest.java
+++ src/test/java/it/univaq/disim/mosaico/wp2/repository/ModelRepositoryTest.java
@@ -21,12 +21,10 @@ public class ModelRepositoryTest {
     
 
     @Test
+    
     public void createModel() {
         Model model = new Model("1", "Model Name", "Model Description", "1.0", "Author Name", "MIT");
-        //when(repository.save(model)).thenReturn(model);
-
         Model savedModel = repository.save(model);
-
         assertNotNull(savedModel);
         assertEquals("1", savedModel.id());
         assertEquals("Model Name", savedModel.name());
@@ -36,42 +34,33 @@ public class ModelRepositoryTest {
     @Test
     public void findModelById() {
         
-        //when(repository.findById("1")).thenReturn(Optional.of(model));
-
         Optional<Model> foundModel = repository.findById("1");
 
         assertTrue(foundModel.isPresent());
         assertEquals("1", foundModel.get().id());
         assertEquals("Model Name", foundModel.get().name());
-        //verify(repository, times(1)).findById("1");
+        
     }
 
     @Test
     public void findModelByIdException() {
-        
-        //when(repository.findById("1")).thenReturn(Optional.of(model));
-
         Optional<Model> foundModel = repository.findById("2");
-
         assertTrue(!foundModel.isPresent());
-        //verify(repository, times(1)).findById("1");
     }
 
     @Test
     public void findModelByName() {
-        //when(repository.findByName("Model Name")).thenReturn(List.of(model));
         var models = repository.findByName("Model Name");
         assertNotNull(models);
         assertEquals(1, models.size());
         assertEquals("Model Name", models.get(0).name());
-        //verify(repository, times(1)).findByName("Model Name");
     }
 
     @Test
     public void deleteModel() {
+        repository.save(model);
         var i = repository.count();
         repository.deleteById("1");
         assertEquals(i-1, repository.count());
-        
     }
 }
